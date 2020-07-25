d7740db6ef601e4e269aa80ef676e8ebac110574
Joao M Dalla Vecchia <59539338+mattdvphx@users.noreply.github.com>
2020-07-25 13:51:51 -0700

    Updates

---
+++ precache-manifest.0ef7664396aa1d0ec21f96a104e79a94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "802813fa80bb5253304b567084744f04",
+    "url": "/react-directory/index.html"
+  },
+  {
+    "revision": "5c4ac8dabfe96b23b380",
+    "url": "/react-directory/static/js/2.f57df2cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-directory/static/js/2.f57df2cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08e14345a9f428d25fe8",
+    "url": "/react-directory/static/js/main.e5f79c72.chunk.js"
+  },
+  {
+    "revision": "deba56ab0f39f48f6548",
+    "url": "/react-directory/static/js/runtime-main.57cf64e2.js"
+  }
+]);
